3cf7b85ee25eaa19e8ca435cd7a0f535768eb00e
KAlecs12 <alexandre.bruno@ynov.com>
2021-06-01 12:19:46 +0200

    maj

--- server.go
+++ server.go
@@ -17,6 +17,9 @@ func main() {
 	http.HandleFunc("/account", accounthandler)
 	http.HandleFunc("/post", posthandler)
 
+	http.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
